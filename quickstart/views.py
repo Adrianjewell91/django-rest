ed8d43d43113c783bfa05e7e992d14a99cc55ce5
Adrian <adrianjewell91@gmail.com>
2017-10-07 14:37:31 -0700

    first c

---
+++ quickstart/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+# Create your views here.
